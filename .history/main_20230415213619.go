809bbfc8e9b7cc79ab9b1720ce010200a1086f6d
twilight2017 <liuzixuanp@163.com>
2023-04-15 21:41:17 +0800

    add config dir

--- .history/main_20230415213619.go
+++
@@ -1,6 +0,0 @@
-package main
-
-import (
-	"github.com/go-oauth2/oauth2/v4/manage"
-	"github.com/go-oauth2/oauth2/v4/server"
-)
--- .history/main_20230415213715.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/go-oauth2/oauth2/v4/manage"
-	"github.com/go-oauth2/oauth2/v4/server"
-)
-
-var srv *server.Server
-var mgr *manage.Manager
