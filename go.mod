706d6f3ab2026f1a5bc1609f1739ac8810563d2e
twilight2017 <liuzixuanp@163.com>
2023-04-17 16:51:35 +0800

    feat: add session pck

--- go.mod
+++ go.mod
@@ -4,10 +4,14 @@ go 1.17
 
 require (
 	github.com/go-oauth2/oauth2/v4 v4.5.2
+	github.com/gorilla/sessions v1.2.1
+	gopkg.in/boj/redistore.v1 v1.0.0-20160128113310-fc113767cd6b
 	gopkg.in/yaml.v2 v2.3.0
 )
 
 require (
+	github.com/garyburd/redigo v1.6.4 // indirect
 	github.com/golang-jwt/jwt v3.2.1+incompatible // indirect
 	github.com/google/uuid v1.1.1 // indirect
+	github.com/gorilla/securecookie v1.1.1 // indirect
 )
--- pkg/session/session.go
+++ pkg/session/session.go
@@ -2,12 +2,70 @@ package session
 
 import (
 	"encoding/gob"
-	"github.com/gprilla/sessions"
-	"githun.com/twilight2017/oauthsso/config"
+	"github.com/gorilla/sessions"
+	"gopkg.in/boj/redistore.v1"
 	"net/http"
 	"net/url"
+	"oauthsso/config"
 )
 
+//用cookie对session进行存储
 var store *sessions.CookieStore
 
 // var store *redistore.RediStore
+func SetUp() {
+	/*url.Values是一个用于存储HTTP请求参数的类型，实现了map[string][]string类型，而map类型是不能被序列化的
+	所以需要在这里进行注册，之后对url.Values()进行序列化和反序列化就可以正常操作了*/
+	gob.Register(url.Values{}) //注册url.Values{}类型，让它可以序列化和反序列化
+
+	//创建一个新的cookie存储器，NewCookieStore会创建一个基于cookie的session存储器
+	//该方法返回了一个实现了session.Store接口的结构体，可用于在后续的代码中进行session的存储和管理
+	store = sessions.NewCookieStore([]byte(config.Get().Session.SecretKey))
+
+	store.Options = &sessions.Options{
+		Path: "/",
+		// session有效期
+		//单位秒
+		MaxAge:   config.Get().Session.MaxAge,
+		HttpOnly: true,
+	}
+	/*
+			store_redis, _ = redistore.NewRediStore(yaml.Cfg.Redis.Defaule.Db, "tcp", yaml.Cfg.Redis.Default.Addr, "", []byte("secret-key"))
+		    if err != nil {
+			log.Fatal(err)
+		}
+	*/
+}
+
+func Get(r *http.Request, name string) (val interface{}, err error) {
+	// Get a session
+	session, err := store.Get(r, config.Get().Session.Name)
+	if err != nil {
+		return
+	}
+	val = session.Values[name]
+	return
+}
+
+func Set(w http.ResponseWriter, r *http.Request, name string, val interface{}) (err error) {
+	// Get a session
+	session, err := store.Get(r, config.Get().Session.Name)
+	if err != nil {
+		return
+	}
+	session.Values[name] = val
+	err = session.Save(r, w)
+	return
+}
+
+func Delete(w http.ResponseWriter, r *http.Request, name string) (err error) {
+	//Get a session
+	session, err := store.Get(r, config.Get().Session.Name)
+	if err != nil {
+		return
+	}
+	delete(session.Values, name)
+	err = session.Save(r, w)
+
+	return
+}
