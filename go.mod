341534c549b0a9e47ef02ba65f8277219784f8c4
twilight2017 <liuzixuanp@163.com>
2023-04-17 17:39:46 +0800

    feat:add ldap config

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module oauthsso
 go 1.17
 
 require (
+	github.com/go-ldap/ldap/v3 v3.4.4
 	github.com/go-oauth2/oauth2/v4 v4.5.2
 	github.com/gorilla/sessions v1.2.1
 	gopkg.in/boj/redistore.v1 v1.0.0-20160128113310-fc113767cd6b
@@ -10,8 +11,11 @@ require (
 )
 
 require (
+	github.com/Azure/go-ntlmssp v0.0.0-20220621081337-cb9428e4ac1e // indirect
 	github.com/garyburd/redigo v1.6.4 // indirect
+	github.com/go-asn1-ber/asn1-ber v1.5.4 // indirect
 	github.com/golang-jwt/jwt v3.2.1+incompatible // indirect
 	github.com/google/uuid v1.1.1 // indirect
 	github.com/gorilla/securecookie v1.1.1 // indirect
+	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
 )
---
+++ pkg/ldap/ldap.go
@@ -0,0 +1,148 @@
+package ldap
+
+import (
+	"crypto/tls"
+	"fmt"
+	ldap "github.com/go-ldap/ldap/v3"
+	"log"
+	"net"
+	"net/url"
+	"oauthsso/config"
+	"strings"
+)
+
+type Session struct {
+	ldapCfg  config.LDAP
+	ldapConn *ldap.Conn
+}
+
+func NewSession(ldapCfg config.LDAP) *Session {
+	return &Session{
+		ldapCfg: ldapCfg,
+	}
+}
+
+//yaml文件中支持多种ldap地址配置方式，这里进行地址格式化处理
+//格式化为protocol://host:port的格式，支持ldap和ldaps协议
+func formatURL(ldapURL string) (string, error) {
+	var protocol, hostport string
+	_, err := url.Parse(ldapURL)
+	if err != nil {
+		return "", fmt.Errorf("parse Ldap Host ERR: %s", err)
+	}
+	if strings.Contains(ldapURL, "://") {
+		splitLdapURL := strings.Split(ldapURL, "://")
+		protocol, hostport = splitLdapURL[0], splitLdapURL[1]
+		if !((protocol == "ldap") || (protocol == "ldaps")) {
+			return "", fmt.Errorf("unknown ldap protocol")
+		}
+	} else {
+		hostport = ldapURL
+		protocol = "ldap"
+	}
+
+	if strings.Contains(hostport, ":") {
+		_, port, err := net.SplitHostPort(hostport)
+		if err != nil {
+			return "", fmt.Errorf("illegal ldap url, error: %v", err)
+		}
+		if port == "636" {
+			protocol = "ldaps"
+		}
+	} else {
+		switch protocol {
+		case "ldap":
+			hostport = hostport + ":389"
+		case "ldaps":
+			hostport = hostport + ":636"
+		}
+	}
+	fLdapURL := protocol + "://" + hostport
+	return fLdapURL, nil
+}
+
+//open session
+//should invoke Close for each Open call
+func (s *Session) Open() error {
+	ldapURL, err := formatURL(s.ldapCfg.URL)
+	if err != nil {
+		return err
+	}
+	splitLdapURL := strings.Split(ldapURL, "://")
+
+	protocol, hostport := splitLdapURL[0], splitLdapURL[1]
+	host, _, err := net.SplitHostPort(hostport)
+	if err != nil {
+		return err
+	}
+
+	log.Println(ldapURL)
+
+	switch protocol {
+	case "ldap":
+		l, err := ldap.Dial("tcp", hostport)
+		if err != nil {
+			return err
+		}
+		s.ldapConn = l
+	case "ldaps":
+		l, err := ldap.DialTLS("tcp", hostport, &tls.Config{ServerName: host, InsecureSkipVerify: true})
+		if err != nil {
+			return err
+		}
+		s.ldapConn = l
+	}
+	return nil
+}
+
+//close current session
+func (s *Session) Close() {
+	if s.ldapConn != nil {
+		s.ldapConn.Close()
+	}
+}
+
+func UserAuthentication(username, password string) (string, error) {
+	s := NewSession(config.Get().LDAP)
+	if err := s.Open(); err != nil {
+		return "", err
+	}
+	defer s.Close()
+
+	// First bind with a read only user
+	if err := s.ldapConn.Bind(s.ldapCfg.SearchDN, s.ldapCfg.SearchPassword); err != nil {
+		return "", err
+	}
+
+	//Search for the given username
+	searchRequest := ldap.NewSearchRequest(
+		s.ldapCfg.BaseDN,
+		ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
+		fmt.Sprintf(s.ldapCfg.Filter, ldap.EscapeFilter(username)),
+		[]string{"dn"},
+		nil,
+	)
+
+	sr, err := s.ldapConn.Search(searchRequest)
+	if err != nil {
+		return "", err
+	}
+
+	if len(sr.Entries) != 1 {
+		return "", fmt.Errorf("用户不存在或不唯一")
+	}
+
+	userdn := sr.Entries[0].DN
+
+	//Bind as the user to verfify their password
+	if err := s.ldapConn.Bind(userdn, password); err != nil {
+		return "", err
+	}
+
+	//Rebind as the read only user for any further queries
+	if err := s.ldapConn.Bind(s.ldapCfg.SearchDN, s.ldapCfg.SearchPassword); err != nil {
+		return "", err
+	}
+
+	return username, nil
+}
